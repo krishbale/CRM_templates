bbde574063a94b0180e316c58b1144a4b5303863
krishbale <sushil.pokharel.7528@gmail.com>
2023-07-23 16:17:12 +0545

    adding resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LeadsModule } from './leads/leads.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { OpportunitiesModule } from './opportunities/opportunities.module';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
-  imports: [],
+  imports: [LeadsModule, AccountsModule, OpportunitiesModule, ContactsModule],
   controllers: [AppController],
   providers: [AppService],
 })
