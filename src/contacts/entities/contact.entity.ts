101d90e8f16fa15d91137898b0d1359ea9fbfd93
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-07-24 17:27:25 +0545

    Add contacts api

--- src/contacts/entities/contact.entity.ts
+++ src/contacts/entities/contact.entity.ts
@@ -1,1 +1,21 @@
-export class Contact {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity({ name: 'contact' })
+export class Contact {
+  @PrimaryGeneratedColumn('uuid', { name: 'id' })
+  id: string;
+
+  @Column({ name: 'name', type: 'varchar', length: 100 })
+  name: string;
+
+  @Column({ name: 'account_name', type: 'varchar', length: 100 })
+  account_name: string;
+
+  @Column({ name: 'phone_number', type: 'varchar', length: 20, nullable: true })
+  phone_number: string;
+
+  @Column({ name: 'email', type: 'varchar', length: 100, unique: true })
+  email: string;
+
+  @Column({ name: 'contact_owner_alias', type: 'varchar', length: 100 })
+  contact_owner_alias: string;
+}
