0156baf13166aba0ed07064bc410cde5c5857dff
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-07-24 17:28:09 +0545

    Add contact and opportunity dtos

--- src/contacts/dto/create-contact.dto.ts
+++ src/contacts/dto/create-contact.dto.ts
@@ -1,1 +1,20 @@
-export class CreateContactDto {}
+import { Transform } from 'class-transformer';
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+export class CreateContactDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  account_name: string;
+
+  @IsString()
+  @IsOptional()
+  phone_number: string;
+
+  @IsEmail()
+  @Transform(({ value }) => value.toLowerCase())
+  email: string;
+
+  @IsString()
+  contact_owner_alias: string;
+}
--- src/opportunities/dto/create-opportunity.dto.ts
+++ src/opportunities/dto/create-opportunity.dto.ts
@@ -1,1 +1,29 @@
-export class CreateOpportunityDto {}
+import {
+  IsDateString,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { opportunity_stage } from '../entities/opportunity.entity';
+
+export class CreateOpportunityDto {
+  @IsString()
+  opportunity_name: string;
+
+  @IsString()
+  account_name: string;
+
+  @IsEnum(opportunity_stage)
+  stage: opportunity_stage;
+
+  @IsDateString()
+  close_date: Date;
+
+  @IsNumber()
+  @IsOptional()
+  probability: number;
+
+  @IsString()
+  opportunity_owner_alias: string;
+}
