dac533d40497f9b01cb2b82884df03194e0184ee
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-07-25 10:58:18 +0545

    Updated dtos with proper checking

--- src/contacts/dto/create-contact.dto.ts
+++ src/contacts/dto/create-contact.dto.ts
@@ -1,10 +1,23 @@
 import { Transform } from 'class-transformer';
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 export class CreateContactDto {
   @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   name: string;
 
   @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   account_name: string;
 
   @IsString()
@@ -12,9 +25,15 @@ export class CreateContactDto {
   phone_number: string;
 
   @IsEmail()
+  @IsNotEmpty()
+  @MinLength(10)
+  @MaxLength(50)
   @Transform(({ value }) => value.toLowerCase())
   email: string;
 
   @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   contact_owner_alias: string;
 }
--- src/opportunities/dto/create-opportunity.dto.ts
+++ src/opportunities/dto/create-opportunity.dto.ts
@@ -1,23 +1,34 @@
 import {
   IsDateString,
   IsEnum,
+  IsNotEmpty,
   IsNumber,
   IsOptional,
   IsString,
+  MaxLength,
+  MinLength,
 } from 'class-validator';
 import { opportunity_stage } from '../entities/opportunity.entity';
 
 export class CreateOpportunityDto {
   @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   opportunity_name: string;
 
   @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   account_name: string;
 
   @IsEnum(opportunity_stage)
+  @IsNotEmpty()
   stage: opportunity_stage;
 
   @IsDateString()
+  @IsNotEmpty()
   close_date: Date;
 
   @IsNumber()
@@ -25,5 +36,8 @@ export class CreateOpportunityDto {
   probability: number;
 
   @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
   opportunity_owner_alias: string;
 }
