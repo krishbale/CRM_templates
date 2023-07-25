bde5ec4f9bd4b847347f84dc68ede6e45dfa8445
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2023-07-25 10:57:54 +0545

    Update contact entity

--- src/contacts/entities/contact.entity.ts
+++ src/contacts/entities/contact.entity.ts
@@ -4,18 +4,34 @@ export class Contact {
   @PrimaryGeneratedColumn('uuid', { name: 'id' })
   id: string;
 
-  @Column({ name: 'name', type: 'varchar', length: 100 })
+  @Column({ name: 'name', type: 'varchar', length: 50, nullable: false })
   name: string;
 
-  @Column({ name: 'account_name', type: 'varchar', length: 100 })
+  @Column({
+    name: 'account_name',
+    type: 'varchar',
+    length: 50,
+    nullable: false,
+  })
   account_name: string;
 
   @Column({ name: 'phone_number', type: 'varchar', length: 20, nullable: true })
   phone_number: string;
 
-  @Column({ name: 'email', type: 'varchar', length: 100, unique: true })
+  @Column({
+    name: 'email',
+    type: 'varchar',
+    length: 50,
+    unique: true,
+    nullable: false,
+  })
   email: string;
 
-  @Column({ name: 'contact_owner_alias', type: 'varchar', length: 100 })
+  @Column({
+    name: 'contact_owner_alias',
+    type: 'varchar',
+    length: 50,
+    nullable: false,
+  })
   contact_owner_alias: string;
 }
